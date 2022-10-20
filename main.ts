c97c3c8f49b9f01f583cab1595ffaadf040343ba
Seppedaddyahw <116261151+Seppedaddyahw@users.noreply.github.com>
2022-10-20 10:18:28 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
-basic.showLeds(`
-    . # # # #
-    # # . # .
-    # # # . .
-    # # # # .
-    . # # # #
-    `)
-basic.showIcon(IconNames.Ghost)
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     music.playMelody("C5 D C5 B E B F E ", 500)
     led.plot(2, 3)
+    basic.showLeds(`
+        . # # # #
+        # # . # .
+        # # # . .
+        # # # # .
+        . # # # #
+        `)
+    basic.showIcon(IconNames.Ghost)
+})
+basic.forever(function () {
+	
 })
